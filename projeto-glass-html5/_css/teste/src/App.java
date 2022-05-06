ac76a4b8bf56043c880decad90aef78c327bb112
davillops <davi_demedeiros@hotmail.com>
2022-05-06 19:41:36 -0300

    MeuProjetoHTML5

---
+++ projeto-glass-html5/_css/teste/src/App.java
@@ -0,0 +1,24 @@
+import javax.swing.SwingWorker;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int x;
+        int[] valorx = new int[3];
+        valorx[0] = 5;
+        
+        for(int i=0;i<valorx.length;i++)
+        System.out.println(valorx[i]); 
+
+        valorx[3] = 13;
+        System.out.println("passei pelo vetor");
+
+
+
+    
+    
+
+    
+
+    }
+}
