5e9a3dfb24ef682767fe7a338aa434bef3fdce30
Davi Lopes <davidemedeiros86@gmail.com>
2022-08-04 22:21:24 -0300

    JavaScript

---
+++ JavaScript/variaveis.js
@@ -0,0 +1,6 @@
+var n1 = Number.parseInt(prompt('Digite um numero'))
+// parseInte força a String virar um numero inteiro
+var n2 = Number.parseFloat(prompt('Digite outro numero'))
+// parseFloat força a String virar um numero c/ ponto flutuante
+var tot = n1 + n2
+    alert(`Valor total é ${tot}`)
---
+++ JavaScript/window.js
@@ -0,0 +1,4 @@
+window.alert('Mensagem de teste') // Janela de alerta
+window.confirm('Você tem certeza?') // Janela de confirmação
+var number = window.prompt('Digite um numero')
+    alert(`O numero é ${number}`)
